7e15e5c5581c43302accb8a12e67a92f5ab48a01
TheBodger <41484656+TheBodger@users.noreply.github.com>
2025-10-08 18:09:04 +0100

    t

--- MMM-ButterMeNoParsnips.js
+++ MMM-ButterMeNoParsnips.js
@@ -69,7 +69,7 @@ Module.register("MMM-ButterMeNoParsnips", {
 			localFileBut.id = "localFileBut";
 			lfbspan = document.createElement("span");
 			t0 = document.createTextNode("Load local files");
-			lfbspan.style.fontSize = "6px";
+			lfbspan.className = "small";
 			lfbspan.appendChild(t0);
 			localFileBut.appendChild(lfbspan)
 
